0f1e4889651d088fabd09ac6ce1fd989c3e54589
GuruYupp <110220639+GuruYupp@users.noreply.github.com>
2023-02-12 13:40:15 +0530

    section changes

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,13 +1,18 @@
-import { ReactNode } from "react"
-import styles from './Section.module.scss'
-import Card from "@/components/card/card"
+import { ReactNode } from "react";
+import styles from "./Section.module.scss";
+import Card from "@/components/card/card";
 
-export default function Section({section}:any):JSX.Element{
-    return <div className={styles.section}>
-        {
-            section?.section?.sectionData?.data.map((card:any,index:any)=>{
-                return <Card key={index} carddata={card}/>
-            })
-        }
+export default function Section({ section }: any): JSX.Element {
+  let cards = section?.section?.sectionData?.data || [];
+  return (
+    <div className={styles.section}>
+      {cards && (
+        <div className= {styles.cards}>
+          {cards.map((card: any, index: any) => {
+            return <Card key={index} carddata={card} />;
+          })}
+        </div>
+      )}
     </div>
+  );
 }
